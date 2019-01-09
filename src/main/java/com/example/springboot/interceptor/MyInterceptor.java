1dfaff67b589e0fa52c15789a60f052f78f8a6e1
jiazhibinjava <2274694434@qq.com>
2019-01-09 16:14:28 +0800

    修改其空白页面的错误

--- src/main/java/com/example/springboot/interceptor/MyInterceptor.java
+++ src/main/java/com/example/springboot/interceptor/MyInterceptor.java
@@ -1,13 +1,11 @@
 package com.example.springboot.interceptor;
 
 import com.example.springboot.pojo.User;
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.lang.reflect.Method;
 
 
 public class MyInterceptor implements HandlerInterceptor {
@@ -28,19 +26,19 @@ public class MyInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        HandlerMethod handlerMethod = (HandlerMethod) handler;
-        Method method = handlerMethod.getMethod();
-        long startTime = (Long) request.getAttribute("requestStartTime");
-        long endTime = System.currentTimeMillis();
-        long executeTime = endTime - startTime;
-        // 打印方法执行时间
-        if (executeTime > 1000) {
-            System.out.println("[" + method.getDeclaringClass().getName() + "." + method.getName() + "] 执行耗时 : "
-                    + executeTime + "ms");
-        } else {
-            System.out.println("[" + method.getDeclaringClass().getSimpleName() + "." + method.getName() + "] 执行耗时 : "
-                    + executeTime + "ms");
-        }
+//        HandlerMethod handlerMethod = (HandlerMethod) handler;
+//        Method method = handlerMethod.getMethod();
+//        long startTime = (Long) request.getAttribute("requestStartTime");
+//        long endTime = System.currentTimeMillis();
+//        long executeTime = endTime - startTime;
+//        // 打印方法执行时间
+//        if (executeTime > 1000) {
+//            System.out.println("[" + method.getDeclaringClass().getName() + "." + method.getName() + "] 执行耗时 : "
+//                    + executeTime + "ms");
+//        } else {
+//            System.out.println("[" + method.getDeclaringClass().getSimpleName() + "." + method.getName() + "] 执行耗时 : "
+//                    + executeTime + "ms");
+//        }
     }
 
     @Override
